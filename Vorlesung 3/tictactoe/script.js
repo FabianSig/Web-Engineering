fd90148a065f9b824ccb98222659bb3485730a27
fabiansig <fabian.sigmund1@gmail.com>
2023-05-24 15:19:48 +0200

    feat: win alert

--- Vorlesung 3/tictactoe/script.js
+++ Vorlesung 3/tictactoe/script.js
@@ -4,16 +4,38 @@ let gameOver = false;
 
 function makeMove(index) {
     
+    checkWin();
     if(board[index] === ''){
         board[index] = currentPlayer;
         document.getElementsByClassName("cell")[index].innerHTML = currentPlayer;
         
+        if(checkWin()) {
+            alert(currentPlayer + " won!");
+        }
         if(currentPlayer === 'X') currentPlayer = 'O';
         else{ currentPlayer = 'X'}
+
 
     }
     
     
+}
+
+function checkWin() {
+
+    if ((board[0] === board[1] && board[1] === board[2] && board[0] !== '') ||
+        (board[3] === board[4] && board[4] === board[5] && board[3] !== '') ||
+        (board[6] === board[7] && board[7] === board[8] && board[6] !== '') ||
+        (board[0] === board[3] && board[3] === board[6] && board[0] !== '') ||
+        (board[1] === board[4] && board[4] === board[7] && board[1] !== '') ||
+        (board[2] === board[5] && board[5] === board[8] && board[2] !== '') ||
+        (board[0] === board[4] && board[4] === board[8] && board[0] !== '') ||
+        (board[2] === board[4] && board[4] === board[6] && board[2] !== '')
+    ) {
+        return true;
+    }
+
+    return false;
 }
 
 function resetGame() {
