0a2cd98af3cb9a1bb59acd680f98e61a9cca40bf
fabiansig <fabian.sigmund1@gmail.com>
2023-06-14 16:34:57 +0200

    feat: added error handling

--- Vorlesung 5/script.js
+++ Vorlesung 5/script.js
@@ -7,6 +7,17 @@ function getRepos(){
             'Authorization' : 'Bearer ' + document.getElementById("password").value
         }
     }
+    ).then(res => {
+
+        if(res.ok){
+            document.getElementById("message").innerHTML = "Success!";
+            return res;
+        }
+        else{
+            throw("Unable to fetch data");
+        }
+
+    }
     ).then(res => res.json())
     .then(res => { 
         res.forEach(element => {
@@ -16,7 +27,7 @@ function getRepos(){
             ul.appendChild(li);
         });
         
-    });
+    }).catch(err => document.getElementById("message").innerHTML = "Error: " + err);
 
 
 }
